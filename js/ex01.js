2dc60c153298da1552ff37a995f4e8a7e9d91974
Vitor Santana <victorssanttana@hotmail.com>
2022-11-26 00:53:44 -0300

    alterações

---
+++ js/ex01.js
@@ -0,0 +1,15 @@
+function alerta() {
+    return "Olá usuário, cuidado"
+}
+console.log(alerta())
+
+function soma(n1,n2) {
+    console.log(`a soma de ${n1} + ${n2} é : ${n1+n2}`)
+}
+soma(1,3)
+
+console.log(`Olá`)
+
+var sorteio=parseInt(Math.random()*3)+1
+
+console.log(sorteio)
